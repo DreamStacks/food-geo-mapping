5c63bb6c8fd135187422abeb71ce6cc1c773dfd5
ihoey <mail@ihoey.com>
2019-05-31 17:56:33 +0800

    feat: add element-ui components

--- src/main.js
+++ src/main.js
@@ -1,19 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+
 import { createRouter } from './router'
 import { createStore } from './store'
 import { sync } from 'vuex-router-sync'
 
 import './styles/index.scss' // global css
 import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
 
 // 关闭生产模式下给出的提示
 Vue.config.productionTip = true
 
 // global loading
-Vue.prototype.$loading = loading => {
-
-}
+Vue.prototype.$loading = loading => {}
 
 export function createApp() {
   const router = createRouter()
