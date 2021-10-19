2e6f0547c731ddf9d5b3bcde2723363972f2f5bf
ihoey <mail@ihoey.com>
2021-10-19 13:55:47 +0800

    feat: 网站底部添加备案信息

--- src/layout/components/Footer.vue
+++ src/layout/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div />
+  <div class="ds-footer"><a href="https://beian.miit.gov.cn/" target="_blank">京ICP备2020033963号</a></div>
 </template>
 <script>
 export default {
@@ -10,4 +10,11 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+.ds-footer {
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 20px;
+  color: #ddd;
+}
 </style>
